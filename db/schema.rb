679868b97f2edc1aaaefa2bb4489a76d3eaec694
Rayanb <rayanbouts@gmail.com>
2015-04-15 19:16:54 -0400

    add stubbed email to db

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 ActiveRecord::Schema.define(version: 20150415203034) do
 
   create_table "users", force: :cascade do |t|
+    t.string   "email",      null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
