83163465417a15b56c38a66ca28611c78eb7b378
Rayanb <rayanbouts@gmail.com>
2015-04-18 14:25:42 -0400

    users controller migrated to api users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,23 @@
 class UsersController < ApplicationController
 
   def index
-    render json: "I am a json object that returns all users"
   end
 
   def show
-    render json: "I am a single user"
   end
 
   def new
-    render json: "Linkedin signup all the time"
   end
 
   def create
-    existing_user = User.find_by(email: params[:email])
-    if existing_user
-      render json: "redirect to news feed"
-    else
-      user = User.create(email: params[:email])
-      puts "redirect to edit_user_path(user.id)"
-      render json: "redirect to edit page"
-    end
   end
 
   def edit
-    render json: "Thats where golf info would be added"
   end
 
   def update
-    render json: "Saves in the database all golf info"
   end
 
   def destroy
-    render json: "destroyed the user"
   end
 end
