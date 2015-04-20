b13d3e15232b8115c35e87b9f99cb61566425125
Rayanb <rayanbouts@gmail.com>
2015-04-19 22:32:08 -0400

    configure linkedin oauth

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
 
   resources :users
 
+
   namespace :api do
-    resources :users, defaults: { format: :json }
+    resources :users, defaults: { format: :json }, except: [:create, :update, :destroy]
   end
+
+  get 'auth/:provider/callback', to: 'users#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'users#destroy', as: 'signout'
+
 end
