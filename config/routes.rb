3beb0db3d402d02ac33984455c437c3ef9efa064
Rayanb <rayanbouts@gmail.com>
2015-04-20 13:10:18 -0400

    user api for creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   get '/', to: "users#index"
 
-  resources :users
+  resources :users, except: [:create, :update, :destroy]
 
 
   namespace :api do
-    resources :users, defaults: { format: :json }, except: [:create, :update, :destroy]
+    resources :users, defaults: { format: :json }
   end
 
-  get 'auth/:provider/callback', to: 'users#create'
+  get 'auth/:provider/callback', to: 'api/users#create'
   get 'auth/failure', to: redirect('/')
-  get 'signout', to: 'users#destroy', as: 'signout'
+  get 'signout', to: 'api/users#destroy', as: 'signout'
 
 end
