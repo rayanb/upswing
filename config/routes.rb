a4ac3aea586e6ab676ff8380e7f7eb70dbd5aa8d
Rayanb <rayanbouts@gmail.com>
2015-04-22 12:26:29 -0400

    reconfigure routes to give some more respomsibility to backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  get '/', to: "users#index"
+  get '/', to: 'welcome#index'
 
-  resources :users, except: [:create, :update, :destroy]
+  resources :users, except: [:update, :destroy]
 
 
   namespace :api do
     resources :users, defaults: { format: :json }
+    resources :friendships, defaults: { format: :json }, only: [:create, :destroy, :update]
   end
 
   get 'auth/:provider/callback', to: 'api/users#create'
   get 'auth/failure', to: redirect('/')
-  get 'signout', to: 'api/users#destroy', as: 'signout'
+  get 'signout', to: 'api/users#logout', as: 'signout'
 
 end
