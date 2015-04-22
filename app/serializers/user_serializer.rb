e96d332e624dcfaf09aa3628e70b449f7265bc84
Rayanb <rayanbouts@gmail.com>
2015-04-22 14:34:20 -0400

    serialize lets picture url pass

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :job, :location, :industry
+  attributes :id, :name, :email, :job, :location, :industry, :picture_url
 end
