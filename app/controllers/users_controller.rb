90ed13ae55a134c5bb140c56f6c4da7a6e9f1f5c
Rayanb <rayanbouts@gmail.com>
2015-04-22 12:26:49 -0400

    more responsibility

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,22 @@ class UsersController < ApplicationController
   def show
   end
 
+  def create
+    user_creation     = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = user_creation[:user].id
+    if user_creation[:message] == "already"
+      redirect_to users_path
+    else
+      redirect_to edit_user_path(session[:user_id])
+    end
+  end
+
   def new
   end
 
   def edit
   end
+
+
 
 end
