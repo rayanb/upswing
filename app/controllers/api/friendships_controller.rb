f394342d2f9e5b9d794e8799747124e461c9aa4b
Rayanb <rayanbouts@gmail.com>
2015-04-25 13:09:21 -0400

    add more responsibility to friendship controller

--- app/controllers/api/friendships_controller.rb
+++ app/controllers/api/friendships_controller.rb
@@ -1,11 +1,24 @@
 class Api::FriendshipsController < ApplicationController
   skip_before_filter :verify_authenticity_token
+  include UsersHelper
+
+  def index
+    friends = ActiveModel::ArraySerializer
+         .new(current_user.friends, each_serializer: UserSerializer)
+    render json: {friends: friends, currentUser: serialized_current_user}
+  end
 
   def create
     user_id   = params[:user_id]
     friend_id = params[:friend_id]
-    Friendship.create(user_id: user_id, friend_id: friend_id)
-    render json: "friendship created"
+    puts FriendRequest.find_by(user_id: user_id, friend_id: friend_id)
+    if FriendRequest.find_by(user_id: user_id, friend_id: friend_id)
+      Friendship.create(user_id: user_id, friend_id: friend_id)
+      render json: 'friendship'
+    else
+      FriendRequest.create(user_id: user_id, friend_id: params[:friend_id])
+      render json: "request"
+    end
   end
 
   def update
