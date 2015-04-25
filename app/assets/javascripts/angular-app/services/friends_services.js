863879d8694aeb76a1e8183e45da224addc6e3c6
Rayanb <rayanbouts@gmail.com>
2015-04-25 13:05:21 -0400

    one single action to create friendship or friend_request as the backend processes the rest

--- app/assets/javascripts/angular-app/services/friends_services.js
+++ app/assets/javascripts/angular-app/services/friends_services.js
@@ -1,15 +1,15 @@
 golfApp.service("Friendship", ['$log', '$http', function($log, $http){
   return {
-    createRequest: function(user, friend){
-      console.log(user)
-      console.log(friend)
-      $http.post('/api/friendrequests?user_id='+user.user.id+'&&friend_id='+friend.id)
-      .then(function(data){
-        console.log(data);
+    create: function(user, friend, callback){
+      $http.post('/api/friendships?user_id='+user.id+'&&friend_id='+friend.id).then(function(data){
+        callback(data)
       })
     },
-    create: function(user, friend){
-      $http.post('/api/friendships?user_id='+user.user.id+'&&friend_id='+friend.id).then(function(data){console.log(data)})
+    getAll: function(callback){
+      $http.get('/api/users/1/friends').then(function(data){
+        console.log(data)
+        callback(data)
+      })
     }
   }
 }])
