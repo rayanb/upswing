6fe6e20573d8a942a65648f0901f18a9a7b8c77b
Rayanb <rayanbouts@gmail.com>
2015-04-27 14:03:58 -0400

    clarify connections

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,4 +1,4 @@
 class FriendRequest < ActiveRecord::Base
-  belongs_to :user, foreign_key: :requesting_user_id
-  belongs_to :friend, foreign_key: :requested_user_id, class_name: "User"
+  belongs_to :user
+  belongs_to :friend, class_name: "User"
 end
