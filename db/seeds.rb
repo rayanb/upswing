12bb3a388f7f3ac6094d27129e5f2069f5823065
Rayanb <rayanbouts@gmail.com>
2015-04-27 14:02:28 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+comp = Industry.create(name: "Computer Software")
 Industry.create(name: "Science")
 Industry.create(name: "Science-fiction")
 Industry.create(name: "Heyya")
+
+require 'date'
+
+20.times do
+  User.create(name: Faker::Name.name, email: Faker::Internet.email, provider: Faker::Internet.url, uid: Faker::Code.isbn, oauth_token: Faker::Code.isbn, location: Faker::Address.city, picture_url: Faker::Avatar.image, industry: comp, job: Faker::Name.title, golfcourse: Faker::Company.name, handicap: rand(1..20), description: Faker::Lorem.paragraph, oauth_expires_at: Date.new)
+end
