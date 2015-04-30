5bb1ae0243798a2abfc12bb829800cec77889df2
Rayanb <rayanbouts@gmail.com>
2015-04-30 10:57:42 -0400

    clean up angular controllers

--- app/assets/javascripts/angular-app/controllers/friendscontroller.js
+++ app/assets/javascripts/angular-app/controllers/friendscontroller.js
@@ -3,15 +3,11 @@ golfApp.controller('FriendsCtrl', [
   function($scope, Friendship, Users){
 
     Users.getCurrent(function(data){
-      console.log("current user")
       $scope.currentUser = data;
-      console.log($scope.currentUser)
     })
 
     Friendship.getAll(function(data){
-      console.log(data)
       $scope.friends = data.friends;
-      console.log($scope.friends)
     })
 
 }])
