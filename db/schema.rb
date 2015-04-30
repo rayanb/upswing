9d662f903949586930e06061314779e14d448707
Rayanb <rayanbouts@gmail.com>
2015-04-30 10:54:50 -0400

    take count of changes in database due to location parameters

--- db/schema.rb
+++ db/schema.rb
@@ -28,21 +28,24 @@ ActiveRecord::Schema.define(version: 20150421151344) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                                                                                                                                               null: false
-    t.string   "email",                                                                                                                                              null: false
-    t.string   "provider",                                                                                                                                           null: false
-    t.string   "uid",                                                                                                                                                null: false
-    t.string   "oauth_token",                                                                                                                                        null: false
-    t.string   "location",                                                                                                                                           null: false
-    t.string   "picture_url",      default: "https://cdn1.iconfinder.com/data/icons/photography-2/512/YPS__human_avatar_portrait_photography_picture_photo-512.png"
-    t.integer  "industry_id",                                                                                                                                        null: false
+    t.string   "name",                                                                                                                                                 null: false
+    t.string   "email",                                                                                                                                                null: false
+    t.string   "provider",                                                                                                                                             null: false
+    t.string   "uid",                                                                                                                                                  null: false
+    t.string   "oauth_token",                                                                                                                                          null: false
+    t.string   "location_city",                                                                                                                                        null: false
+    t.string   "location_state"
+    t.string   "location_longitude"
+    t.string   "location_latitude"
+    t.string   "picture_url",        default: "https://cdn1.iconfinder.com/data/icons/photography-2/512/YPS__human_avatar_portrait_photography_picture_photo-512.png"
+    t.integer  "industry_id",                                                                                                                                          null: false
     t.string   "job"
     t.string   "golfcourse"
     t.string   "handicap"
     t.text     "description"
     t.datetime "oauth_expires_at"
-    t.datetime "created_at",                                                                                                                                         null: false
-    t.datetime "updated_at",                                                                                                                                         null: false
+    t.datetime "created_at",                                                                                                                                           null: false
+    t.datetime "updated_at",                                                                                                                                           null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@ Industry.create(name: "Heyya")
 require 'date'
 
 20.times do
-  User.create(name: Faker::Name.name, email: Faker::Internet.email, provider: Faker::Internet.url, uid: Faker::Code.isbn, oauth_token: Faker::Code.isbn, location: Faker::Address.city, picture_url: Faker::Avatar.image, industry: comp, job: Faker::Name.title, golfcourse: Faker::Company.name, handicap: rand(1..20), description: Faker::Lorem.paragraph, oauth_expires_at: Date.new)
+  User.create(name: Faker::Name.name, email: Faker::Internet.email, provider: Faker::Internet.url, uid: Faker::Code.isbn, oauth_token: Faker::Code.isbn, location_city: Faker::Address.city, location_state: Faker::Address.state, location_longitude: Faker::Address.longitude, location_latitude: Faker::Address.latitude, picture_url: Faker::Avatar.image, industry: comp, job: Faker::Name.title, golfcourse: Faker::Company.name, handicap: rand(1..20), description: Faker::Lorem.paragraph, oauth_expires_at: Date.new)
 end
