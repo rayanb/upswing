8e41fc701708fb76889abf7fcb2a47f4eb131c30
Rayanb <rayanbouts@gmail.com>
2015-05-04 11:59:44 -0400

    add longitude and latitude to user's location

--- db/migrate/20150415203034_create_users.rb
+++ db/migrate/20150415203034_create_users.rb
@@ -6,10 +6,10 @@ class CreateUsers < ActiveRecord::Migration
       t.string :provider, null: false
       t.string :uid, null: false
       t.string :oauth_token, null: false
-      t.string :location_city, null: false
+      t.string :location_city
       t.string :location_state
-      t.string :location_longitude
-      t.string :location_latitude
+      t.float :location_longitude
+      t.float :location_latitude
       t.string :picture_url, default: "https://cdn1.iconfinder.com/data/icons/photography-2/512/YPS__human_avatar_portrait_photography_picture_photo-512.png"
       t.references :industry, null: false
       t.string :job
