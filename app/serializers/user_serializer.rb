d437412c7a8a5f3a9e8bcf1bd1e4eb175b2afb17
Rayanb <rayanbouts@gmail.com>
2015-05-20 16:48:47 -0400

    add specialty as a user attribute

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :job, :location_city, :location_state, :location_longitude, :location_latitude, :industry, :picture_url, :educations, :jobs, :handicap, :golfcourse, :golf_associations, :rounds_played, :member_at, :favorite_club, :availability
-
-      #   t.string :golfcourse, default: "N/A"
-      # t.string :handicap, default: "N/A"
-      # t.string :golf_associations, default: "N/A"
-      # t.string :rounds_played, default: "N/A"
-      # t.string :member_at, default: "N/A"
-      # t.string :favorite_club, default: "N/A"
+  attributes :id, :name, :email, :job, :location_city, :location_state, :location_longitude, :location_latitude, :industry, :picture_url, :specialty, :jobs, :handicap, :golfcourse, :golf_associations, :rounds_played, :member_at, :favorite_club, :availability, :provider
 end
