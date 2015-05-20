0d9bcc560550938d25ecb7e7a949934b4a62cb13
Rayanb <rayanbouts@gmail.com>
2015-05-20 16:49:53 -0400

    add facebook auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :linkedin,  ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'], :scope => 'r_emailaddress r_fullprofile'
+  provider :linkedin,  ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'], :scope => 'r_emailaddress r_basicprofile'
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
